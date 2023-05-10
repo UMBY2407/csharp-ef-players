8aef4e2c55a2d4045b3e6ed0d1da880bec3c724f
UMBY2407 <umby2407@gmail.com>
2023-05-10 15:01:21 +0200

    Added Player.cs as Entity Annotation

---
+++ csharp-ef-players/Player.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("Player")]
+
+public class Player
+{
+	[Key]
+	public int PlayerId { get; set; }
+	public string? Name { get; set; }
+	public string Surname { get; set; }
+	public int Score { get; set; }
+	public int NumberOfMatchesPlayed { get; set; }
+	public int NumberOfMatchesWon { get; set; }
+}
