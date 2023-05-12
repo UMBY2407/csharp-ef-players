8bed9d9fa3a5fd9bf9f1207e98c92880aad6e988
UMBY2407 <umby2407@gmail.com>
2023-05-12 15:21:59 +0200

    Added NewTeam1 and NewPlayer1,2,3 in Program.cs

---
+++ csharp-ef-players/Migrations/20230512131748_UpdateEntity.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace csharp_ef_players.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "player",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TeamId",
+                table: "player",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_player_PlayerId",
+                table: "player",
+                column: "PlayerId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_player_TeamId",
+                table: "player",
+                column: "TeamId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_player_team_TeamId",
+                table: "player",
+                column: "TeamId",
+                principalTable: "team",
+                principalColumn: "TeamId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_player_team_TeamId",
+                table: "player");
+
+            migrationBuilder.DropIndex(
+                name: "IX_player_PlayerId",
+                table: "player");
+
+            migrationBuilder.DropIndex(
+                name: "IX_player_TeamId",
+                table: "player");
+
+            migrationBuilder.DropColumn(
+                name: "TeamId",
+                table: "player");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "player",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
--- csharp-ef-players/Program.cs
+++ csharp-ef-players/Program.cs
@@ -4,29 +4,54 @@ using csharp_ef_players;
 using (SportContext db = new SportContext())
 {
 	//Create
+	Team Team1 = new Team("Juventus", "Torino", "Allegri", "Bianco-Nero");
 	/*
-	Player newPlayer = new Player ("Alessandro", "Del Piero", 705, 387);
-	db.Add(newPlayer);
+	db.Add(Team1);
 	db.SaveChanges();
-
-	Console.WriteLine(newPlayer.ToString());ù
+	Console.WriteLine("Squadra creata:");
+	Console.WriteLine(Team1.ToString());
+	*/
+	Player newPlayer1 = new Player ("Leonardo", "Bonucci", 500, 319);
+	db.Add(newPlayer1);
+	db.SaveChanges();
+	//Console.WriteLine($"Giocatore aggiunto alla Squadra: " + Team1.Name);
+	Console.WriteLine(newPlayer1.ToString());
+	/*
+	Player loadedPlayer = db.Players.Where(player => m.Surname == "Bonucci").First();
+	db.Remove(loadedPlayer);
+	db.SaveChanges();
 	*/
 
+	Player newPlayer2 = new Player("Federico", "Chiesa", 89, 54);
+	db.Add(newPlayer2);
+	db.SaveChanges();
+	//Console.WriteLine($"Giocatore aggiunto alla Squadra: " + Team1.Name);
+	Console.WriteLine(newPlayer2.ToString());
+	/*
+	Player loadedPlayer = db.Players.Where(player => m.Surname == "Chiesa").First();
+	db.Remove(loadedPlayer);
+	db.SaveChanges();
+	*/
+
+	Player newPlayer3 = new Player("Da Silva", "Danilo", 159, 97);
+	db.Add(newPlayer3);
+	db.SaveChanges();
+	//Console.WriteLine($"Giocatore aggiunto alla Squadra: " + Team1.Name);
+	Console.WriteLine(newPlayer3.ToString());
+	/*
+	Player loadedPlayer = db.Players.Where(player => m.Surname == "Danilo").First();
+	db.Remove(loadedPlayer);
+	db.SaveChanges();
+	*/
 
 	// Read
-	/*
+
 	Console.WriteLine("Recupero lista di giocatori");
 	List<Player> players = db.Players.OrderBy(players => players.Name).ToList<Player>();
 
 	//Update
-	newPlayer.Name = "Gianluigi";
-	db.SaveChanges();
-	*/
-
-	//Delete
 	/*
-	Player loadedPlayer = db.Players.Where(m => m.PlayerId == 7).First();
-	db.Remove(loadedPlayer);
+	newPlayer1.Name = "Gianluigi";
 	db.SaveChanges();
 	*/
 }
